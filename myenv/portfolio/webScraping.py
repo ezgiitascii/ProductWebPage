e1fb37585494529e64a7b4336ee89e3e5d9ecfc1
ezgiitascii <65920875+ezgiitascii@users.noreply.github.com>
2020-09-09 23:46:30 +0300

    get yesterday's data's

--- myenv/portfolio/webScraping.py
+++ myenv/portfolio/webScraping.py
@@ -1,17 +1,31 @@
 import requests
 import pandas as pd
+import datetime
+from datetime import  date, timedelta
+from matplotlib import pyplot as plt
 
 
+#get yesterday date
+today = date.today()
+yesterday = today - datetime.timedelta(days=1)
+ara= yesterday.strftime("%Y")+"-"+ yesterday.strftime("%m")+"-"+yesterday.strftime("%d")
+
+#request and response
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
 }
-
-response = requests.get("https://api.turib.com.tr/api/getbulletin/2020-09-03",headers=headers)
-
+response = requests.get("https://api.turib.com.tr/api/getbulletin/"+ ara ,headers=headers)
 j = response.json()
 
+#save in dataframe
 df = pd.DataFrame.from_dict(j["DailyList"])
+df = df.set_index("Product")
 
-#df = df.set_index("")
+#virtualization
+df1 = df.drop(["Id","Type", "PrevClose", "TradeDate", "ISIN", "ProductCode", "ProductClass", "Low", "High", "NumberOfTrades", "TradedVolume", "TradedValue"], axis = 1)
+df1.plot( subplots = True )
 
-print(df)
+plt.legend()
+plt.xlabel("Products")
+#plt.title("Günlük Bülten")
+plt.show()
