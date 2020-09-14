1f52ce54be4c55ab44066565ed38bc893421e753
ezgiitascii <65920875+ezgiitascii@users.noreply.github.com>
2020-09-14 23:10:14 +0300

    sample.html added

--- myenv/portfolio/webScraping.py
+++ myenv/portfolio/webScraping.py
@@ -4,25 +4,25 @@ import datetime
 from datetime import  date, timedelta
 from matplotlib import pyplot as plt
 
-
 #get yesterday date
-today = date.today()
-yesterday = today - datetime.timedelta(days=1)
-ara= yesterday.strftime("%Y")+"-"+ yesterday.strftime("%m")+"-"+yesterday.strftime("%d")
+yesterday = date.today() - datetime.timedelta(days=1)
+dateStr = yesterday.strftime("%Y") + "-" + yesterday.strftime("%m") + "-" + yesterday.strftime("%d")
+print(dateStr)
 
 #request and response
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
 }
-response = requests.get("https://api.turib.com.tr/api/getbulletin/"+ ara ,headers=headers)
-j = response.json()
+response = requests.get("https://api.turib.com.tr/api/getbulletinsummary/"+ dateStr, headers=headers)
+jsonData = response.json()
 
 #save in dataframe
-df = pd.DataFrame.from_dict(j["DailyList"])
+df = pd.DataFrame.from_dict(jsonData["InstantlyList"])
 df = df.set_index("Product")
 
 #virtualization
-df1 = df.drop(["Id","Type", "PrevClose", "TradeDate", "ISIN", "ProductCode", "ProductClass", "Low", "High", "NumberOfTrades", "TradedVolume", "TradedValue"], axis = 1)
+#df1 = df.drop(["Id","Type", "PrevClose", "TradeDate", "ISIN", "ProductCode", "ProductClass", "Low", "High", "NumberOfTrades", "TradedVolume", "TradedValue"], axis = 1)
+df1 = df.drop(["TradeDate", "ProductType", "ProductGroup", "NumberOfTrades", "TradedVolume", "TradedValue", "Anlasmali"], axis = 1)
 df1.plot( subplots = True )
 
 plt.legend()
--- myenv/products/views.py
+++ myenv/products/views.py
@@ -4,7 +4,11 @@ from django.http import HttpResponse
 
 from .models import Product
 
+
 # Create your views here.
 def index(request):
-    products = Product.objects
+    products = Product.objects.all()
     return render(request, 'products/index.html', {'products':products})
+
+def dashboard(request):
+    return render(request, 'dashboard/sample.html')
